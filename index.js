1b30f21bf9656079443bb7c6b26b37a86e31f315
Povilas <167984240+Povilas88@users.noreply.github.com>
2024-07-20 20:27:16 +0300

    class Car created

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 import { Dog } from "./js/Dog.js";
+import { Car } from "./js/Cars.js";
 
 const rex = new Dog("Rex", 'black', 3);
 console.log(rex);
@@ -22,3 +23,76 @@ console.log(brisius.lostLeg());
 console.log(brisius.birthday());
 console.log(brisius.birthday());
 console.log(brisius.birthday());
+
+console.clear();
+
+/*
+Instructions
+class -> Car(Automobilis)
+
+Savybės:
+
+-   pavadinimas(Audi)
+    - modelis(80)
+    - spalva
+    - kuro bako talpa(litrais)
+    - vidutinės kuro sąnaudos 100km
+    - ar įjungtas varyklis(default: false)
+    - greitis(default: 0)
+
+Metodai:
+
+-   ijungti varykli
+    - ijungto varykli dar karto ijungti negalima, sugadinti starteri
+    - isjungti varykli
+    - isjungto varyklio isjungti negalima...
+-   pradeti vaziuoti(greitis tiesiog tampa ne nulinis ir sunaudoja 2x litrus kuro sanaudu)
+    - vaziuoti(naudoja 1x litro kuro sanaudu)
+    - sustoti(greitis tiesiog tampa nulinis)
+    - kiek liko kuro ?
+    - uzpilti kuro baka(kiek litrais)
+*/
+
+const automobilis = new Car('Audi', 80, 'Silver', 68, 7.8);
+
+console.log(automobilis.name);
+console.log(automobilis.model);
+
+console.log(automobilis.turnEngineOff());
+console.log(automobilis.turnEngineOn());
+console.log(automobilis.turnEngineOn());
+console.log(automobilis.turnEngineOff());
+console.log(automobilis.startDrive());
+console.log(automobilis.turnEngineOn());
+console.log(automobilis.startDrive());
+console.log(automobilis.startDrive());
+console.log(automobilis.driving());
+console.log(automobilis.driving());
+console.log(automobilis.driving());
+console.log(automobilis.driving());
+console.log(automobilis.driving());
+console.log(automobilis.driving());
+console.log(automobilis.driving());
+console.log(automobilis.driving());
+console.log(automobilis.startDrive());
+console.log(automobilis.stopDrive());
+console.log(automobilis.addFuel());
+console.log(automobilis.turnEngineOff());
+console.log(automobilis.addFuel());
+console.log(automobilis.addFuel());
+console.log(automobilis.startDrive());
+console.log(automobilis.turnEngineOn());
+console.log(automobilis.startDrive());
+console.log(automobilis.stopDrive());
+console.log(automobilis.stopDrive());
+console.log(automobilis.addFuel());
+
+
+
+
+
+
+
+
+
+
---
+++ js/Cars.js
@@ -0,0 +1,78 @@
+export class Car {
+    constructor(name, model, color, fuelCapacity, avgFuelConsumption) {
+        this.name = name;
+        this.model = model;
+        this.color = color;
+        this.fuelCapacity = fuelCapacity;
+        this.avgFuelConsumption = avgFuelConsumption;
+        this.isEngineOn = false;
+        this.speed = 0;
+        this.fuel = fuelCapacity;
+        this.firstDrive = true;
+    }
+
+    turnEngineOn() {
+        return this.isEngineOn ? "Car engine was already on."
+            : (this.isEngineOn = true, 'Car engine turned on.');
+    }
+
+    turnEngineOff() {
+        return this.speed !== 0 ? 'Stop the car first.'
+            : this.isEngineOn ? (this.isEngineOn = false, 'Car engine turned off.')
+                : "Car engine was already off.";
+    }
+
+    startDrive() {
+        if (this.isEngineOn === false) {
+            return 'Turn the engine on first.';
+        } else if (this.speed !== 0) {
+            return 'Car is already driving';
+        } else if (this.fuel <= 0) {
+            return 'Add fuel first';
+        }
+        else {
+            this.firstDrive = true;
+            this.speed++;
+            this.avgFuelConsumption *= 2;
+            this.fuel -= this.avgFuelConsumption;
+            return `Car starts to drive, fuel left ${this.fuel}`;
+        }
+    }
+
+    driving() {
+        if (this.speed === 0) {
+            return 'Start driving first.';
+        } else if (this.fuel <= 0) {
+            this.speed = 0;
+            return 'Add fuel first.';
+        }
+        else if (this.firstDrive) {
+            this.avgFuelConsumption /= 2;
+            this.firstDrive = false;
+        }
+        (this.fuel -= this.avgFuelConsumption) <= 0 ? this.fuel = 0 : this.fuel;
+        return `You are driving, fuel left ${this.fuel.toFixed(2)}`;
+    }
+
+    stopDrive() {
+        if (this.speed === 0) {
+            return 'You already stopped.';
+        } else {
+            this.speed = 0;
+            return 'You stopped.';
+        }
+    }
+
+    addFuel() {
+        if (this.speed !== 0) {
+            return 'Stop the car first.';
+        } else if (this.isEngineOn) {
+            return 'Turn off the engine first.';
+        } else if (this.fuel === this.fuelCapacity) {
+            return 'Fuel tank already topped off.';
+        } else {
+            this.fuel = this.fuelCapacity - this.fuel;
+            return `You added ${this.fuel.toFixed(2)} liters of fuel.`;
+        }
+    }
+}
